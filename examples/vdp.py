646f61bab15afeceed64f49a5fbb56f1d1ccb1d7
Javier Arroyo <javier.arroyo@wedoco.io>
2024-08-09 16:16:40 +0200

    Use matplotlib to simplify script.

--- examples/vdp.py
+++ examples/vdp.py
@@ -1,27 +1,30 @@
-import numpy as np
-from optimo.plots import plot_from_def
+import matplotlib.pyplot as plt
 from optimo.model import OptimoModel
 
+# Compile and transfer the Modelica model
 mo = OptimoModel()
-mo.transfer_model(model='vdp', force_recompile=False)
+mo.transfer_model(model="vdp", force_recompile=False)
 
-
-plot_def = {}
-plot_def['x'] = {}
-plot_def['x']['values'] = mo.tgrid
-plot_def['x']['title'] = 'Time (s)'
-plot_def['Angle (rad)'] = {}
-plot_def['Angle (rad)']['vars'] = ['x1', 'x2']
-plot_def['Outputs'] = {}
-plot_def['Outputs']['vars'] = ['objectiveIntegrand']
-plot_def['Inputs'] = {}
-plot_def['Inputs']['vars'] = ['u']
-
+# Simulate
 res_sim_df = mo.simulate()
-plot_from_def(plot_def, res_sim_df, show=False, save_to_file=True, filename='plot_sim.html')
 
+# Plot 
+fig, axs = plt.subplots(3, 1)
+res_sim_df[["x1","x2"]].plot(ax=axs[0], title="States", legend=True)
+res_sim_df["objectiveIntegrand"].plot(ax=axs[1], title="Objective", legend=True)
+res_sim_df["u"].plot(ax=axs[2], title="Input", legend=True)
+plt.show()
+
+# Optimize 
 mo.initialize_optimization()
-mo.define_optimization(constraints={'u':(-1, 0.75)}, 
-                       objective_terms=['objectiveIntegrand'])
+mo.define_optimization(constraints={"u":(-1, 0.75)}, 
+                       objective_terms=["objectiveIntegrand"])
 res_ocp_df = mo.optimize()
-plot_from_def(plot_def, res_ocp_df, show=False, save_to_file=True, filename='plot_ocp.html')
+
+# Plot 
+fig, axs = plt.subplots(3, 1)
+res_ocp_df[["x1","x2"]].plot(ax=axs[0], title="States", legend=True)
+res_ocp_df["objectiveIntegrand"].plot(ax=axs[1], title="Objective", legend=True)
+res_ocp_df["u"].plot(ax=axs[2], title="Input", legend=True)
+plt.show()
+
